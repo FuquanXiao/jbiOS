223f033a13b52bf2f610aa5db62bec83d68ffe5f
apple <apple@apple.com>
2019-02-25 14:13:16 +0800

    no message

---
+++ gitDeepStudy/JBAPPHelper/People.h
@@ -0,0 +1,13 @@
+//
+//  People.h
+//  JBAPPHelper
+//
+//  Created by apple on 2019/2/25.
+//  Copyright © 2019年 apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface People : NSObject
+
+@end
---
+++ gitDeepStudy/JBAPPHelper/People.m
@@ -0,0 +1,13 @@
+//
+//  People.m
+//  JBAPPHelper
+//
+//  Created by apple on 2019/2/25.
+//  Copyright © 2019年 apple. All rights reserved.
+//
+
+#import "People.h"
+
+@implementation People
+
+@end
